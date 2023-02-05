f69bd3056ef5dc80c97045319cf9af8c4185eda2
jeenklynn <46195953+jeenklynn@users.noreply.github.com>
2023-02-05 17:40:06 +0300

    xd

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Jeenklynn</title>
+        <meta name="description" content="Welcome to my space, this is my personal website.">
+        <meta name="keywords" content="jeenklynn">
+        <?php include("static/head.htm") ?>
+    </head>
+    <body>
+        <?php include("static/nav.htm") ?>
+        <?php include("contents/index.htm") ?>
+    </body>
+    <footer>
+        <?php include("static/footer.htm") ?>
+    </footer> 
+</html>
---
+++ sources/scripts/skarnium.js
@@ -0,0 +1,67 @@
+/* 
+    slideshow
+            */
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+/*
+    accordion
+            */
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
+
+/*
+    tabs
+        */
+function openTab(evt, tabName) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+
+    document.getElementById(tabName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
