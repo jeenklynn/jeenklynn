e19b5deb96bb7aa07c5e1524aaec1e01c18b8ef8
jeenklynn <46195953+jeenklynn@users.noreply.github.com>
2023-02-09 01:54:50 +0300

    update

---
+++ pages/blogs.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Bloglar</title>
+        <meta name="description" content="this is my blogs page">
+        <meta name="keywords" content="jeenklynn">
+        <?php include("../static/head.htm") ?>
+    </head>
+    <body>
+        <?php include("../static/nav.htm") ?>
+        <?php include("../contents/blogs.htm") ?>
+    </body>
+    <footer>
+        <?php include("../static/footer.htm") ?>
+    </footer> 
+</html>
---
+++ pages/projects.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Projeler</title>
+        <meta name="description" content="this is my projects page">
+        <meta name="keywords" content="jeenklynn">
+        <?php include("../static/head.htm") ?>
+    </head>
+    <body>
+        <?php include("../static/nav.htm") ?>
+        <?php include("../contents/projects.htm") ?>
+    </body>
+    <footer>
+        <?php include("../static/footer.htm") ?>
+    </footer> 
+</html>
